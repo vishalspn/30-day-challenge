6ae965f409c3a284fd4bbde1d831b778ccf1f363
vishal <vishal.chaturvedi@bidchat.com>
2019-01-18 17:27:09 +0530

    user test

---
+++ backend/User.py
@@ -0,0 +1,10 @@
+class User:
+
+    def __int__(self):
+        print("Empty constructor")
+
+    def userLogin(self, name):
+        print('login successfull ' + name)
+
+p1 = User()
+p1.userLogin("vishal")
